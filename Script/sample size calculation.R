c0869f5b20fa1c84c60241c8a8e2053bf810025e
Dani Kim <k2258693@kcl.ac.uk>
2023-10-02 13:35:20 +0100

    Added section of power calculation for survival analysis (Cox PH regression)

--- Script/sample size calculation.R
+++ Script/sample size calculation.R
@@ -1,4 +1,5 @@
 library(WebPower)
+library(powerSurvEpi)
 
 # logistic regression (univariate)
 WebPower::wp.logistic(p0=0.165, p1=0.201, alpha=0.05, power=0.80, alternative="two.sided", family="Bernoulli")
@@ -6,6 +7,36 @@ WebPower::wp.logistic(p0=0.165, p1=0.201, alpha=0.05, power=0.80, alternative="t
 WebPower::wp.logistic(p0=0.132, p1=0.5, alpha=0.05, power=0.80, alternative="two.sided", family="Bernoulli")
 
 WebPower::wp.logistic(p0=0.28, p1=0.39, alpha=0.05, power=0.80, alternative="two.sided", family="Bernoulli")
+
+
+# cox PH regression
+powerSurvEpi::powerEpi.default()
+  # n = total number of subjects
+  # theta = posulated hazard ratio
+  # p = proportion of subjects taking the value one for the covariate of interest
+  # psi = proportion of subjects died of disease of interest
+  # rho2 = square of the correlation between the covariate of interest and the other covariate
+  # alpha = type 1 error rate
+
+#example
+powerEpi.default(n = 2000,
+                 theta = 1.25,
+                 p = 0.02,
+                 psi = 0.3,
+                 rho2 = 0.3^2,
+                 alpha = 0.05)
+
+# Sample size calculation for survival analysis
+
+hr=1.5       # hazard ratio 
+hr0=1         # hypothesised hazard ratio under null
+pE=0.3        # overall probability of event occurring within study period
+pA=0.08       # proportions of sample size allotted to the two groups
+alpha=0.05    # type 1 error
+beta=0.20     # type 2 error (1-beta) = power
+(n=((qnorm(1-alpha/2)+qnorm(1-beta))/(log(hr)-log(hr0)))^2/(pA*(1-pA)*pE))
+ceiling(n)
+(Power=pnorm((log(hr)-log(hr0))*sqrt(n*pA*(1-pA)*pE)-qnorm(1-alpha/2)))
 
 # prevalence study
 
@@ -17,7 +48,7 @@ sample_calc_prev = function(p, z, d){
   return(n)
 }
 
-sample_calc_prev(0.02, 1.96, 0.05)
+sample_calc_prev(0.3, 1.96, 0.05)
 sample_calc_prev(0.08, 1.96, 0.05)
 
 sample_calc_prev(0.1, 1.96, 0.05)
